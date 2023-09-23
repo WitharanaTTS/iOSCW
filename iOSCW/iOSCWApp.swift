f98bdb04145c45bfbdea4ce45dff2b3d076dd39a
ThushaniSankalpana <witharanathushani.sankalpana@gmail.com>
2023-09-23 20:32:39 +0530

    Initial Commit

---
+++ iOSCW/iOSCWApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCWApp.swift
+//  iOSCW
+//
+//  Created by Lakna Attigala on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct iOSCWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
